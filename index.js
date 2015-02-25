6fe725d7d10f55fb57b35a0d4c6ae7a36e166fe3
Santtu Virolainen <santtu.virolainen@solita.fi>
2015-02-25 15:37:07 +0200

    Added query functionality

--- index.js
+++ index.js
@@ -7,13 +7,15 @@
 
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var Logger = require('mongodb').Logger;
 var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 
 var app = express(); // luodaan uusi express applikaatio
 app.use(bodyParser.json()); // lisää requestin käsittelijä -middleware
 
-app.use(serveStatic('client/', {'index': ['index.html', 'index.htm']}));
+app.use(serveStatic('client/', {'index': ['index.html', 'index.htm']})); // tarjoillaan client app
 
 
 MongoClient.connect('mongodb://127.0.0.1/moviedb', function (err, db) {
@@ -23,12 +25,15 @@ MongoClient.connect('mongodb://127.0.0.1/moviedb', function (err, db) {
 	init(db);
 });
 
+//Logger.setLevel('debug');
+//Logger.filter('class', ['Cursor']);
+
 function init(db) {
 
-	var collection = db.collection('movies');
+	var collection = db.collection('movies'); // 'taulu'
 
 	// endpoint kaikkien elokuvien hakemiseen
-	app.get('/movies', function (req, res, next) {
+	app.get('/movies', function (req, res, next) { // next on seuraava middleware käsittelijä
 	  	collection.find().toArray(function (err, results) {
 	  		if (err) {
 				next(err); // HTTP 500
@@ -40,11 +45,25 @@ function init(db) {
 	});
 
 	app.get('/search', function (req, res) {
-	  	console.log(req.query);
+	  	collection.find({title: req.query.title}).toArray(function (err, results) {
+	  		if (err) {
+				next(err); // HTTP 500
+				return;
+			}
+
+			res.send(results);
+	  	});
 	});
 
-	app.get('/movies/:movieId', function (req, res) {
-	  	console.log(req.param);
+	app.get('/movies/:id', function (req, res) {
+	  	collection.findOne({_id: ObjectId(req.params.id)}, {}, function (err, result) {
+	  		if (err) {
+				next(err); // HTTP 500
+				return;
+			}
+
+			res.send(result);
+	  	});
 	});
 
 	app.post('/movies', function (req, res) {
@@ -65,7 +84,7 @@ function init(db) {
 	});
 }
 
-// asetetaan applikatio kuuntelemaan porttia 9000 ja
+// asetetaan applikaatio kuuntelemaan porttia 9000 ja
 // tulostetaan viesti kun se on valmis vastaanottamaan kyselyitä
 app.listen(9000, function () {
 	console.log('Movie API listening port 9000');
